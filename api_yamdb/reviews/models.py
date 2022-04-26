9973bf90e9ea5781c7082bc0454a0fdf897be7d3
Ольга Воронюк <helga-61rus@yandex.ru>
2022-04-26 15:44:20 +0300

    Добавила модели Categories и Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -37,3 +37,39 @@ class Comment(models.Model):
 class Titles(models.Model):
     name = models.TextField()
     year = models.IntegerField()
+
+
+class Сategories(models.Model):
+    """Категории: фильмы, книги, музыка и т.д."""
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='Категория',
+        help_text='Укажите категорию'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    def __str__(self):
+        return self.slug
+
+
+class Genres(models.Model):
+    """Жанры произведений"""
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='Жанр',
+        help_text='Укажите жанр'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    def __str__(self):
+        return self.slug
