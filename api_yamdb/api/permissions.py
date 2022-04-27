e7ada619673a4bca7b1ce4e69b22bfc950d4f100
Дмитрий Тростянский <tr.dm.al@yandex.ru>
2022-04-28 00:52:44 +0300

    Удалили лишние пробелы

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,7 +7,7 @@ class IsAuthor(permissions.BasePermission):
     def has_permission(self, request, view):
 
         return request.user.is_authenticated
-    
+
     def has_object_permission(self, request, view, obj):
 
         return obj.author == request.user
