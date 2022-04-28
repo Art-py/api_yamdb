a265d4c2bfeac80ce86d133735808f5582fd49a8
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-04-28 19:42:46 +0700

    Implement some serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -6,17 +6,34 @@ from reviews.models import Category, Genre,  Comment
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class SimpleUserSerializer(serializers.ModelSerializer):
+    """Сериализатор с username и email, используется для регистрации"""
     class Meta:
         model = User
         fields = [
             'username',
             'email',
+        ]
+        extra_kwargs = {'email': {'required': True}}
+
+
+class BasicUserSerializer(SimpleUserSerializer):
+    """Сериализатор для пользователей с ролью не равной ADMIN."""
+    class Meta(SimpleUserSerializer.Meta):
+        fields = SimpleUserSerializer.Meta.fields + [
             'first_name',
             'last_name',
             'bio',
             'role',
         ]
+        read_only_fields = ['role']
+
+
+class FullUserSerializer(SimpleUserSerializer):
+    """Сериализатор для пользователей с ролью ADMIN."""
+    class Meta(BasicUserSerializer.Meta):
+        read_only_fields = BasicUserSerializer.Meta.read_only_fields[:]
+        read_only_fields.remove('role')
 
 
 class CategorySerializer(serializers.ModelSerializer):
