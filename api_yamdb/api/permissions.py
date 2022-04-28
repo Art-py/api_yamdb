e9e7e7466bfdce7f41139533275145748050e765
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-04-29 03:16:37 +0700

    Fix IsAdmin and IsModerator permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,16 +5,16 @@ User = get_user_model()
 
 
 class IsAuthor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        return (
+            request.user.is_authenticated
+            and request.user.role == User.ADMIN
+        )
 
     def has_object_permission(self, request, view, obj):
         return request.user.role == User.ADMIN
@@ -22,7 +22,10 @@ class IsAdmin(permissions.BasePermission):
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        return (
+            request.user.is_authenticated
+            and request.user.role == User.MODERATOR
+        )
 
     def has_object_permission(self, request, view, obj):
         return request.user.role == User.MODERATOR
@@ -30,7 +33,4 @@ class IsModerator(permissions.BasePermission):
 
 class IsReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS
