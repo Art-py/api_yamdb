28c582e189b2786fed9d86404fa3d9fdefdf16d9
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-04-28 16:40:34 +0700

    Implement UserSerializer class

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,22 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from reviews.models import Category, Genre,  Comment
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        ]
 
 
 class CategorySerializer(serializers.ModelSerializer):
