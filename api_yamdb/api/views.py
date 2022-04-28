cb0ca92ac37b19b994b2b4b2bc96b9c86a3d70cc
Дмитрий Тростянский <tr.dm.al@yandex.ru>
2022-04-29 00:30:47 +0300

    Дописал вьюхи CommentViewSet, ReviewViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -15,20 +15,28 @@ from reviews.models import Category, Genre, Comment, Review, Title
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthor, IsAdmin, IsModerator, IsReadOnly,)
+    permission_classes = (IsReadOnly | IsAuthor | IsAdmin | IsModerator,)
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         return review.comments.filter(review=review)
 
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (IsAuthor, IsAdmin, IsModerator, IsReadOnly,)
+    permission_classes = (IsReadOnly | IsAuthor | IsAdmin | IsModerator,)
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.filter(title=title)
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
