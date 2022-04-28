30ca3015a612ad3180d3ac975911ac6004accf5d
Ольга Воронюк <helga-61rus@yandex.ru>
2022-04-29 00:35:45 +0300

    поправила вьюсеты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -32,7 +32,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAdmin, IsReadOnly,)
+    permission_classes = (IsReadOnly | IsAdmin, )
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
@@ -41,7 +41,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class GenreViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAdmin, IsReadOnly,)
+    permission_classes = (IsReadOnly | IsAdmin, )
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
