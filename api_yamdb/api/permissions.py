f48a6a8f1106ad0ac4e4f0445376a2d4ea38cffa
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-04-29 23:36:00 +0700

    Fix IsAdmin permission
    Now user with is_superuser=True is equal to admin

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -17,13 +17,13 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and request.user.role == User.ADMIN
+            and (request.user.role == User.ADMIN or request.user.is_superuser)
         )
 
     def has_object_permission(self, request, view, obj):
         return (
             request.user.is_authenticated
-            and request.user.role == User.ADMIN
+            and (request.user.role == User.ADMIN or request.user.is_superuser)
         )
 
 
