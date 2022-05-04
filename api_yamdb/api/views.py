48623e009afdfa8992bbba111554347389b845eb
Дмитрий Тростянский <tr.dm.al@yandex.ru>
2022-05-04 12:44:04 +0300

    Убрали дублирование во вью Комментариев и Отзывов

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -100,12 +100,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsReadOnly | IsAuthor | IsAdmin | IsModerator,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
-        return review.comments.filter(review=review)
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())
+
+    def get_review(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review)
+        return review
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -113,12 +115,14 @@ class ReviewViewSet(viewsets.ModelViewSet):
     permission_classes = (IsReadOnly | IsAuthor | IsAdmin | IsModerator,)
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        return title.reviews.filter(title=title)
+        return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
+
+    def get_title(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        serializer.save(author=self.request.user, title=title)
+        return title
 
 
 class TitleViewSet(viewsets.ModelViewSet):
